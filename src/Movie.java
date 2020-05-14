8c677933a3c6d053f30316834a29b2df8d2b2da7
hottek <dev@mardybum.de>
2020-05-14 17:43:34 +0200

    STEP: Replace conditional with polymorphism

--- src/Movie.java
+++ src/Movie.java
@@ -3,18 +3,93 @@ public class Movie {
     public static final int REGULAR = 0;
     public static final int NEW_RELEASE = 1;
     private String title;
-    private int priceCode;
+    private Price priceCode;
     public Movie(String newtitle, int newpriceCode) {
         title = newtitle;
-        priceCode = newpriceCode;
+        setPriceCode(newpriceCode);
     }
     public int getPriceCode() {
-        return priceCode;
+        return priceCode.getPriceCode();
     }
     public void setPriceCode(int arg) {
-        priceCode = arg;
+        switch (arg) {
+            case REGULAR:
+                priceCode = new RegularPrice();
+                break;
+            case CHILDRENS:
+                priceCode = new ChildrensPrice();
+                break;
+            case NEW_RELEASE:
+                priceCode = new NewReleasePrice();
+                break;
+            default:
+                throw new IllegalArgumentException("Incorrect Price Code");
+        }
     }
     public String getTitle (){
         return title;
     };
+
+    double getCharge(int daysRented) {
+        return priceCode.getCharge(daysRented);
+    }
+
+    int getFrequentRenterPoints(int daysRented) {
+        return priceCode.getFrequentRenterPoints(daysRented);
+    }
+
+    abstract class Price {
+        abstract int getPriceCode();
+        abstract double getCharge(int daysRented);
+        int getFrequentRenterPoints(int daysRented) {
+            return 1;
+        }
+    }
+
+    class ChildrensPrice extends Price {
+        @Override
+        int getPriceCode() {
+            return Movie.CHILDRENS;
+        }
+
+        @Override
+        double getCharge(int daysRented) {
+            double result = 1.5;
+            if (daysRented > 3)
+                result += (daysRented - 3) * 1.5;
+            return result;
+        }
+    }
+
+    class NewReleasePrice extends Price {
+        @Override
+        int getPriceCode() {
+            return Movie.NEW_RELEASE;
+        }
+
+        @Override
+        double getCharge(int daysRented) {
+            return daysRented * 3;
+        }
+
+        @Override
+        int getFrequentRenterPoints(int daysRented) {
+            return (daysRented > 1) ? 2 : 1;
+        }
+    }
+
+    class RegularPrice extends Price {
+        @Override
+        int getPriceCode() {
+            return Movie.REGULAR;
+        }
+
+        @Override
+        double getCharge(int daysRented) {
+            double result = 2;
+            if (daysRented > 2)
+                result += (daysRented - 2) * 1.5;
+            return result;
+        }
+    }
 }
